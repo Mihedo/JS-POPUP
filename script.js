5c83107b0b0638db7602f55892192b7568fdd26c
Mihedo <123761016+Mihedo@users.noreply.github.com>
2023-03-08 21:26:47 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let popup = document.querySelector(".popup");
+let buttonTown = document.querySelector(".button");
+
+buttonTown.addEventListener("click", () => {
+  popup.style.display = "flex";
+  conatinerBackground.style.display = "inline";
+});
